83ab443872aa2c2af5f56d9b778461a9c3490116
joyaoun05-ux <joyaoun05@gmail.com>
2026-04-13 13:30:03 +0300

    feat: add background music and button sound music and how to play panel

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,33 +1,98 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("UI")]
     public TMP_Text highScoreUI;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject howToPlayPanel;
 
-    string newGameScene = "SampleScene";
+    [Header("Scene")]
+    [SerializeField] private string newGameScene = "SampleScene";
 
-    void Start()
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private float delayBeforeAction = 0.2f;
+
+    private void Start()
     {
         int highScore = SaveLoadManager.Instance.LoadHighScore();
         highScoreUI.text = $"Top Wave Survived: {highScore}";
+
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(true);
+
+        if (howToPlayPanel != null)
+            howToPlayPanel.SetActive(false);
     }
 
-   public void StartNewGame()
+    public void StartNewGame()
     {
-        SceneManager.LoadScene(newGameScene);
+        StartCoroutine(PlaySoundAndLoadScene());
     }
 
     public void ExistApplication()
     {
+        StartCoroutine(PlaySoundAndExit());
+    }
+
+    public void OpenHowToPlay()
+    {
+        PlayClickSound();
+
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(false);
+
+        if (howToPlayPanel != null)
+            howToPlayPanel.SetActive(true);
+    }
+
+    public void CloseHowToPlay()
+    {
+        PlayClickSound();
+
+        if (howToPlayPanel != null)
+            howToPlayPanel.SetActive(false);
+
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(true);
+    }
+
+    private IEnumerator PlaySoundAndLoadScene()
+    {
+        PlayClickSound();
+
+        yield return new WaitForSecondsRealtime(delayBeforeAction);
+
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    private IEnumerator PlaySoundAndExit()
+    {
+        PlayClickSound();
+
+        yield return new WaitForSecondsRealtime(delayBeforeAction);
+
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-
 #else
         Application.Quit();
 #endif
     }
 
-
+    private void PlayClickSound()
+    {
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+        else
+        {
+            Debug.LogError("AudioSource or ClickSound not assigned!");
+        }
+    }
 }
